fe9829e00f3980147503fcf7d62f3c15129f4bba
Cayden <swarg1119@gmail.com>
2026-03-14 14:45:26 +0530

    Added if/else statements, for loop, while loop

--- compiler/Parser.java
+++ compiler/Parser.java
@@ -2,6 +2,7 @@ package compiler;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static compiler.TokenType.*;
@@ -33,13 +34,93 @@ public class Parser {
     }
 
     private Stmt statement(){
+
+        if(match(FOR))
+            return forStatement();
+
+        if(match(IF))
+                return ifStatement();
+
         if(match(PRINT))
             return printStatemtent();
+
+        if(match(WHILE))
+            return whileStatement();
 
         if(match(LEFT_BRACE))
             return new Stmt.Block(block());
 
         return expressionStatement();
+    }
+
+    private Stmt forStatement(){
+        consume(LEFT_PAREN, "Expect '(' afte 'for'.");
+
+        //Initializer Block
+        Stmt initializer;
+        //for empty initializer i.e. no initializer is present
+        if(match(SEMICOLON))
+            initializer=null;
+        //initializer is defined using a variable like var i=0
+        else if(match(VAR))
+            initializer=varDeclaration();
+        //Iniitializer is defined but only initialized like i=0
+        else
+            initializer=expressionStatement();
+
+        //Condition Block
+        Expr condition=null;
+        if(!check(SEMICOLON))
+            condition=expression();
+
+        consume(SEMICOLON, "Expect ';' after loop condition.");
+
+        //Increment Block
+        Expr increment=null;
+        if(!check(SEMICOLON))
+            increment=expression();
+
+        consume(RIGHT_PAREN, "Expect ')' after for clauses.");
+        Stmt body=statement();
+
+        if(increment!=null){
+            body=new Stmt.Block(
+                Arrays.asList(
+                    body,
+                    new Stmt.Expression(increment)
+                )
+            );
+        }
+
+        if(condition==null)
+            condition=new Expr.Literal(true);
+
+        body=new Stmt.While(condition, body);
+
+        if(initializer!=null){
+            body=new Stmt.Block(
+                Arrays.asList(
+                    initializer,
+                    body
+                )
+            );
+        }
+
+        return body;
+
+    }
+
+    private Stmt ifStatement(){
+        consume(LEFT_PAREN,"Expect '(' after 'if'.");
+        Expr condition=expression();
+        consume(RIGHT_PAREN, "Expect ')' after 'if' condition.");
+
+        Stmt thenBranch=statement();
+        Stmt elseBranch=null;
+        if(match(ELSE))
+            elseBranch=statement();
+
+        return new Stmt.If(condition, thenBranch, elseBranch);
     }
 
     private Stmt printStatemtent(){
@@ -57,6 +138,15 @@ public class Parser {
 
         consume(SEMICOLON, "Expect ';' after variable declaration.");
         return new Stmt.Var(name, initializer);
+    }
+
+    private Stmt whileStatement(){
+        consume(LEFT_PAREN, "Expect '(' after 'while'.");
+        Expr condition=expression();
+        consume(RIGHT_PAREN, "Expect ')' after condition.");
+        Stmt body=statement();
+
+        return new Stmt.While(condition, body);
     }
 
     private Stmt expressionStatement(){
@@ -93,6 +183,30 @@ public class Parser {
         
         return expr;
     } 
+
+    private Expr or(){
+        Expr expr=and();
+
+        while(match(OR)){
+            Token operator=previous();
+            Expr right=and();
+
+            expr=new Expr.Logical(expr, operator, right);
+        }
+        return expr;
+    }
+
+    private Expr and(){
+        Expr expr=equality();
+
+        while(match(AND)){
+            Token opearator=previous();
+            Expr right=equality();
+            expr=new Expr.Logical(expr, opearator, right);
+        }
+
+        return expr;
+    }
 
     //2nd Rule
     //Grammar for equality is equality-> comparison(("==" || "!=")comparison)*
