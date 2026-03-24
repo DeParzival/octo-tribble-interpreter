77fbbc057ac32b629b94db72d457a7b5a86fda83
Cayden <swarg1119@gmail.com>
2026-03-25 02:05:51 +0530

    Implemented chunk, bytecode writing and disassembler with OP_CONSTANT

---
+++ vm/chunk.h
@@ -0,0 +1,25 @@
+#ifndef clox_name_h
+#define clox_name_h
+
+#include "common.h"
+#include "value.h"
+
+typedef enum{
+    OP_RETURN,
+    OP_CONSTANT
+} OpCode;
+
+typedef struct{
+    int count; //number of allocated bytes that are actually in use
+    int capacity; //Total allocated space i.e. how many bytes we can store before resizing
+    uint8_t* code; //pointer to a dynamically allocated array of bytes, each byte represents a piece of instruction i.e bytecode
+    int* lines;
+    ValueArray constants; //
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+void freeChunk(Chunk* chunk);
+int addConstant(Chunk* chunk, Value value);
+
+#endif
